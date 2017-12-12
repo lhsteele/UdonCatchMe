fcdb4b8f4a8f9e184e6489d7108961b0beba8c5a
Lisa Steele <lisahsteele@gmail.com>
2017-12-12 13:02:12 -0800

    Added the app icon image and registration instruction label to the UserRegistration scene.

--- SKLab1/UserRegistration.swift
+++ SKLab1/UserRegistration.swift
@@ -12,15 +12,26 @@ import SpriteKit
 class UserRegistration: SKScene {
     
     let background = SKSpriteNode(imageNamed: "GameOverBackground")
+    let usernameSceneImage = SKSpriteNode(imageNamed: "UsernameSceneImage")
+    let createUsernameLabel = SKSpriteNode(imageNamed: "CreateUsernameLabel")
     var playableRect: CGRect
     var deviceWidth = UIScreen.main.bounds.width
     var deviceHeight = UIScreen.main.bounds.height
+    
+    
     
     override func sceneDidLoad() {
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         background.zPosition = -1
         addChild(background)
+        
+        usernameSceneImage.position = CGPoint(x: size.width / 2, y: (deviceHeight - deviceHeight) + usernameSceneImage.size.height)
+        addChild(usernameSceneImage)
+        
+        createUsernameLabel.position = CGPoint(x: size.width / 2, y: size.height / 2 + createUsernameLabel.size.height * 2)
+        addChild(createUsernameLabel)
+        
     }
     
     override init(size: CGSize) {
