a3a8a99c829faf33f63aaf95d5a846ecd9d2cda2
Lisa Steele <lisahsteele@gmail.com>
2017-12-12 14:14:11 -0800

    Username text field added to UserRegistration scene. Need to add functionality to text field.

--- SKLab1/UserRegistration.swift
+++ SKLab1/UserRegistration.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 import SpriteKit
+import UIKit
 
-class UserRegistration: SKScene {
+class UserRegistration: SKScene, UITextFieldDelegate {
     
     let background = SKSpriteNode(imageNamed: "GameOverBackground")
     let usernameSceneImage = SKSpriteNode(imageNamed: "UsernameSceneImage")
     let createUsernameLabel = SKSpriteNode(imageNamed: "CreateUsernameLabel")
+    var usernameTextField: UITextField!
     var playableRect: CGRect
     var deviceWidth = UIScreen.main.bounds.width
     var deviceHeight = UIScreen.main.bounds.height
@@ -32,6 +34,17 @@ class UserRegistration: SKScene {
         createUsernameLabel.position = CGPoint(x: size.width / 2, y: size.height / 2 + createUsernameLabel.size.height * 2)
         addChild(createUsernameLabel)
         
+        
+        
+    }
+    
+    override func didMove(to view: SKView) {
+        guard let view = self.view else {return}
+        let originX = (size.width - size.width / 2) / 2
+        usernameTextField = UITextField(frame: CGRect.init(x: originX, y: size.height / 2 - 30, width: size.width / 2, height: 30))
+        customize(textField: usernameTextField, placeholder: "Username")
+        view.addSubview(usernameTextField)
+        usernameTextField.addTarget(self, action: #selector(UserRegistration.textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
     }
     
     override init(size: CGSize) {
@@ -40,6 +53,26 @@ class UserRegistration: SKScene {
         let playableMargin = (size.width - playableWidth) / 2.0
         playableRect = CGRect(x: playableMargin, y: 0, width: playableWidth, height: size.height)
         super.init(size: size)
+    }
+    
+    func customize(textField: UITextField, placeholder: String, isSecureTextEntry: Bool = false) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 30))
+        textField.leftView = paddingView
+        textField.keyboardType = UIKeyboardType.default
+        textField.leftViewMode = UITextFieldViewMode.always
+        textField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSForegroundColorAttributeName : UIColor.gray])
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 4.0
+        textField.textColor = .darkGray
+        textField.isSecureTextEntry = isSecureTextEntry
+        textField.delegate = self
+    }
+    
+    func textFieldDidChange(textField: UITextField) {
+        print ("TextFieldDidChange")
     }
     
     required init?(coder aDecoder: NSCoder) {
