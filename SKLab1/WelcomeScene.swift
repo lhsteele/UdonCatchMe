f1440d68e9ea2fe5ef8db3f5f92ffe5118841a1e
Lisa Steele <lisahsteele@gmail.com>
2017-12-20 14:35:13 -0800

    Added a button on the Welcome scene to lead to the Game Rules scene.

--- SKLab1/WelcomeScene.swift
+++ SKLab1/WelcomeScene.swift
@@ -13,9 +13,15 @@ class WelcomeScene: SKScene {
     var welcomeLabel: SKLabelNode!
     let beginButtonTexture = SKTexture(imageNamed: "BeginButton")
     var beginButton: SKSpriteNode! = nil
+    let gameRulesTexture = SKTexture(imageNamed: "GameRulesButton")
+    var gameRulesButton: SKSpriteNode! = nil
     var background = SKSpriteNode(imageNamed: "WelcomeBackground")
     let gameInstructionsSpeechBubbleTexture = SKTexture(imageNamed: "GameInstructionsSpeechBubble")
     var speechBubble: SKSpriteNode! = nil
+    
+    var playableRect: CGRect
+    var deviceWidth = UIScreen.main.bounds.width
+    var deviceHeight = UIScreen.main.bounds.height
     
     override func didMove(to view: SKView) {
         beginButton = SKSpriteNode(texture: beginButtonTexture)
@@ -25,6 +31,10 @@ class WelcomeScene: SKScene {
         speechBubble = SKSpriteNode(texture: gameInstructionsSpeechBubbleTexture)
         speechBubble.position = CGPoint(x: size.width/2, y: (size.height/2 + beginButton.size.height) - speechBubble.size.height/2)
         addChild(speechBubble)
+        
+        gameRulesButton = SKSpriteNode(texture: gameRulesTexture)
+        gameRulesButton.position = CGPoint(x: (size.width - size.width) + 75, y: (size.height - size.height) + 30)
+        addChild(gameRulesButton)
         
     }
     
@@ -36,8 +46,13 @@ class WelcomeScene: SKScene {
     }
     
     override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = deviceHeight / deviceWidth
+        let playableWidth = size.height / maxAspectRatio
+        let playableMargin = (size.width - playableWidth) / 2.0
+        playableRect = CGRect(x: playableMargin, y: 0, width: playableWidth, height: size.height)
         super.init(size: size)
         name = "WelcomeScene"
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -46,6 +61,9 @@ class WelcomeScene: SKScene {
             if beginButton.contains(location) {
                 let scene = GameScene(size: size)
                 self.view?.presentScene(scene)
+            } else if gameRulesButton.contains(location) {
+                let scene2 = GameRulesScene(size: size)
+                self.view?.presentScene(scene2)
             }
         }
     }
