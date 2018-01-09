581b323069d15389014997a5a27858630a3fe13c
Lisa Steele <lisahsteele@gmail.com>
2018-01-09 11:29:43 -0800

    Testing game over scene. If high score to save, Leaderboard button takes you to scene prompting username to save score. If no high score to save, leaderboard button simply takes you to leaderboard screen.

--- SKLab1/GameOverScene.swift
+++ SKLab1/GameOverScene.swift
@@ -61,11 +61,12 @@ class GameOverScene: SKScene {
                 let scene = GameScene(size: size)
                 self.view?.presentScene(scene)
             } else if leaderboardButton.contains(location) {
+                /*
                 let leaderboardScene = Leaderboard(size: self.size)
                 self.view?.presentScene(leaderboardScene)
                 //let userRegistrationScene = UserRegistration(size: self.size)
                 //self.view?.presentScene(userRegistrationScene)
-                /*
+                */
                 if GameScene.gameWonBoolean == true && GameScene.itsADraw == false {
                     let userRegistrationScene = UserRegistration(size: self.size)
                     self.view?.presentScene(userRegistrationScene)
@@ -73,7 +74,7 @@ class GameOverScene: SKScene {
                     let leaderboardScene = Leaderboard(size: self.size)
                     self.view?.presentScene(leaderboardScene)
                 }
-                */
+ 
             }
         }
     }
